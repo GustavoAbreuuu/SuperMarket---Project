dfb0ca9bc81697ce5c54712ccdf769be3a645c56
GustavoAbreuuu <gustavo.abreu2009@gmail.com>
2026-02-19 05:18:28 -0300

    fix: adiciona module.exports no server.js para a Vercel

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.get('/', (req, res) => {
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
